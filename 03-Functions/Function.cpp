6bb39556c1f70ed7496823c81345b283598e4bbc
Aditya <adityaboura09@gmail.com>
2026-06-30 18:52:55 +0530

    Update Function.cpp

---
+++ 03-Functions/Function.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int multiply(int a,int b);
+using namespace std;
+int main() {
+     int a=4,b=8;
+     cout<<multiply(a,b);
+     return 0;
+}
+int multiply(int a,int b) {
+     return a*b;
+}
