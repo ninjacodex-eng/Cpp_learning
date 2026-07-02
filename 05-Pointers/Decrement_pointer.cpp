2004977b229b42ec1f51058d9c88b1fa3dc4aec4
Aditya <adityaboura09@gmail.com>
2026-07-02 13:50:40 +0530

    Add Decrement_pointer.cpp to demonstrate pointers

---
+++ 05-Pointers/Decrement_pointer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num[4]={1,2,3,4};
+    int *p;
+    p=num;
+    for (int i=4; i>0; i--) {
+        cout<<"This is address : "<<p<<endl;
+        cout<<"This is value : "<<*p<<endl;
+        p++;
+    }
+    return 0;
+}
