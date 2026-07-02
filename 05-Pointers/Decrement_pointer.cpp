5f58d5f57b25912068c177b53f7d294de585e18d
Aditya <adityaboura09@gmail.com>
2026-07-02 14:44:23 +0530

    Change pointer initialization and decrement method

--- 05-Pointers/Decrement_pointer.cpp
+++ 05-Pointers/Decrement_pointer.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main() {
     int num[4]={1,2,3,4};
     int *p;
-    p=num;
+    p=&num[3];
     for (int i=4; i>0; i--) {
         cout<<"This is address : "<<p<<endl;
         cout<<"This is value : "<<*p<<endl;
-        p++;
+        p--;
     }
     return 0;
 }
