4a327f65316deb74902ac828ad92b10bf4ca6ab9
Aditya Boura <adityaboura09@gmail.com>
2026-07-22 00:01:10 +0530

    Add pushing_a_value.cpp to demonstrate vector usage

---
+++ 09-STL/pushing_a_value.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+vector<int>num={44,23,28};
+num.push_back(78);
+cout<<"So the outcome is : ";
+for(int i : num){
+            cout<<i<<" ";
+    };
+return 0;
+};
