a8b4c7ec547e911b77543219fb55202344da4231
Aditya Boura <adityaboura09@gmail.com>
2026-07-22 12:49:24 +0530

    Implement character vector and push_back example
    Added a vector of characters and pushed a new character 'j' into it.

---
+++ 09-STL/Vectors/pushing_char.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+        vector<char>u={'t','g','h'};
+        u.push_back('j');
+       cout<<"These are the values : ";
+       for(char i : u){
+                 cout<<i<<" ";  
+       };
+      return 0;
+};
