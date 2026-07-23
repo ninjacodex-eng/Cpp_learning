13b32090851b631a209c4fe363fad58926c39f75
Aditya Boura <adityaboura09@gmail.com>
2026-07-23 15:03:43 +0530

    Implement basic queue operations in push_in_queue.cpp

---
+++ 09-STL/Queue/push_in_queue.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    queue<int>v;
+    v.push(68);
+    v.push(49);
+    v.push(28);
+    cout<<"The first element : "<<v.front()<<endl;
+    cout<<"The last element : "<<v.back();
+    return 0;
+};
