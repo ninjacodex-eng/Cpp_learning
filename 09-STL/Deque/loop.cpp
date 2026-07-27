46fac9dc6d9a3677d16a4f708f9ea4ce786316ae
Aditya Boura <adityaboura09@gmail.com>
2026-07-27 19:56:23 +0530

    Add loop.cpp to display deque contents
    Implement a simple program to display medicines from a deque by index.

---
+++ 09-STL/Deque/loop.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include<string>
+using namespace std;
+int main(){
+        deque<string>q={"pentacid","antacid","lizol"};
+    cout<<"These are medicines by index : "<<endl;
+         for(int i=0; i<q.size();i++){
+               cout<<q[i]<<endl;
+    };
+return 0;
+};
