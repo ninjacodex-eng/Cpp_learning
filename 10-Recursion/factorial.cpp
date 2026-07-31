572b5cb1cd158ea027b1046206508d1f1f5e7dbf
Aditya Boura <adityaboura09@gmail.com>
2026-07-31 15:48:17 +0530

    Add recursive factorial function in C++

---
+++ 10-Recursion/factorial.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int fact(int num){
+    if (num==0)
+        return 1;
+    else
+        return num * fact(num-1);
+};
+int main(){
+    int num;
+    cout<<"Enter the no : ";
+    cin>>num;
+    cout<<fact(num)<<endl;
+    return 0;
+};
