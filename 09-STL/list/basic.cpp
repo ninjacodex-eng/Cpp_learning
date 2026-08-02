69efd667747eddb4ac0ca8545eeeb8cf6b7d36fb
Aditya Boura <adityaboura09@gmail.com>
2026-08-02 20:48:13 +0530

    Add basic list operations in basic.cpp

---
+++ 09-STL/list/basic.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    list<int>l;
+     l.push_back(50);
+    l.push_back(90);
+    l.push_back(36);
+    for(int i : l){
+        cout<<i<<" ";
+    }
+    return 0;
+}
